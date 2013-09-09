9a8a40fa1618de1b943c74779c7f14c276fcf9bb
LiuRongtao <fiftyk@qq.com>
2013-09-09 15:56:35 +0800

    initialize...

---
+++ README.md
@@ -0,0 +1,7 @@
+# Google WMTS
+
+使用 WMTS 协议访问 Google 底图
+
+## 使用场景
+
+* 使用 ArcMap 加载 WMTS 协议的栅格底图，进行矢量化
---
+++ python/wmts.py
@@ -0,0 +1,65 @@
+#coding=utf-8
+'''
+Created on 2012-7-19
+将google Map转化为WMST服务
+@author: fiftyk
+'''
+import tornado.ioloop
+import tornado.web
+import sqlite3,StringIO,math
+from tornado.web import GZipContentEncoding
+
+class WMTSHandler(tornado.web.RequestHandler):
+    TileMatrix = '''<TileMatrix>
+                <ows:Identifier>%s</ows:Identifier>
+                <ScaleDenominator>%s</ScaleDenominator>
+                <TopLeftCorner>-20037508.342787 20037508.342787</TopLeftCorner>
+                <TileWidth>256</TileWidth>
+                <TileHeight>256</TileHeight>
+                <MatrixWidth>%s</MatrixWidth>
+                <MatrixHeight>%s</MatrixHeight>
+            </TileMatrix>'''
+    
+    url_pattern = {
+        "street":"http://mt0.google.cn/vt/lyrs=m@169000000&hl=zh-CN&gl=cn&x=%s&y=%s&z=%s&s=",
+        "satellite":"http://khm1.google.com/kh/v=114&src=app&x=%s&y=%s&z=%s",
+    }
+    
+    def initialize(self):
+        pass
+
+    def get(self):
+        #?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities
+        #TileRow=1&TileCol=0
+        row = self.get_argument("TileRow",-1)
+        col = self.get_argument("TileCol",-1)
+        level = self.get_argument("TileMatrix",-1)
+        layer = self.get_argument("layer","street")
+        
+        if row != -1 and col != -1 and level != -1:
+            url = self.url_pattern.get(layer)%(col,row,level.replace("EPSG:3785:",""))
+            #print url
+            self.redirect(url)
+        else:
+            self.set_header("Content-Type", "text/xml;charset=utf-8")
+            TileMatrix_List = ""
+        
+            for i in range(20):
+                level = str(i)
+                scale = str(559082264.0287178 /math.pow(2,i))
+                width = str(1 << i)
+                TileMatrix_List += self.TileMatrix%(level,scale,width,width)
+            
+            self.xml = open("wmts.xml").read()%TileMatrix_List
+            self.write(self.xml)
+
+    post = get
+
+GZipContentEncoding.CONTENT_TYPES.add("image/png")
+
+application = tornado.web.Application([
+    (r"/wmts",WMTSHandler)
+],"",None,gzip=True)
+
+application.listen(5555)
+tornado.ioloop.IOLoop.instance().start()
